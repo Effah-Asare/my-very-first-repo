3682b45a1d255b4ceae6797c2db0bae8fa3fb460
Effah-Asare <kaeffah.asare@gmail.com>
2024-02-16 19:06:25 +0000

    added savings.py

---
+++ savings.py
@@ -0,0 +1,6 @@
+amount_saved = 0
+
+while amount_saved <= 100:
+    amount_to_save = int(input("How much will you save: "))
+    amount_saved += amount_to_save
+print("We have managed to save up to 100")

ae3fe687e4ef1e8ba1aa443054624023233ae1e6
Effah-Asare <kaeffah.asare@gmail.com>
2024-02-22 18:49:56 +0000

    modified file

--- savings.py
+++ savings.py
@@ -4,8 +4,9 @@ while amount_saved <= 100:
     money_made = int(input("How much did you make today: "))
     amount_to_save = money_made * 0.4
     if money_made < 20:
-        print(f"Your earnings for today is not sufficient. We were able to save {amount_saved} for now. However, Let's pause on savings for now")
+        print(f"Your earnings for today is not sufficient. We were able to save {amount_saved} for now. However, Let us pause on savings for now")
         break
     amount_saved += amount_to_save
 print(f"We have managed to save up to {amount_saved}")
+
 
